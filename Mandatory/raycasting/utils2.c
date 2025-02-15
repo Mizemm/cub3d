8412551a940abf1131477a708749ae6da1088f69
AmineSalmi01 <amine.salmi.solicode@gmail.com>
2025-02-15 20:52:05 +0100

    raycast modified

--- Mandatory/raycasting/utils2.c
+++ Mandatory/raycasting/utils2.c
@@ -6,7 +6,7 @@
 /*   By: asalmi <asalmi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 13:50:09 by asalmi            #+#    #+#             */
-/*   Updated: 2025/02/15 00:40:35 by asalmi           ###   ########.fr       */
+/*   Updated: 2025/02/15 00:44:55 by asalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,12 +56,12 @@ void mouse_hook(double xpos, double ypos, void *param)
 void rays_direction(t_ray *ray)
 {
 	double angle = ray->ray_angle;
-	if (is_facing_up(angle))
+	if (is_facing_up(angle) && ray->foundHorz)
 		ray->foundNO = true;
-	if (is_facing_down(angle))
+	if (is_facing_down(angle) && ray->foundHorz)
 		ray->foundSO = true;
-	if (is_facing_right(angle))
+	if (is_facing_right(angle) && ray->foundHorz)
 		ray->foundEA = true;
-	if (is_facing_left(angle))
+	if (is_facing_left(angle) && ray->foundHorz)
 		ray->foundWE = true;
 }
